b82a65c43d103d3a5791784515b50fa294b5190a
Brandon Nguyen <brandonnguyen@brandons-macbook-air.local>
2017-12-26 13:22:29 -0500

    Added buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,19 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController : UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // Test
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!;
+        print("Touched \(digit) digit");
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
