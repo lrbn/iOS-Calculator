31b6eea02a947f600ad23eb680c5e5e2b523c49e
Brandon Nguyen <brandonnguyen@brandons-macbook-air.local>
2017-12-27 22:31:18 -0500

    Implemented calculator display functionality when tapping on digits

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController : UIViewController {
 
+    @IBOutlet weak var display: UILabel?;
+    var userIsInTheMiddleOfTyping: Bool = false;
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!;
-        print("Touched \(digit) digit");
+        if (userIsInTheMiddleOfTyping) {
+            let textCurrentlyInDisplay = display!.text!;
+            display!.text = textCurrentlyInDisplay + digit;
+        } else {
+            display!.text = digit;
+            userIsInTheMiddleOfTyping = true;
+        }
     }
 }
 
